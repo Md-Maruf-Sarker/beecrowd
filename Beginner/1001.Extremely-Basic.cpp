3cf6ded00afe6d0c159694268847154e3c66fa32
Md. Maruf Sarker <78826405+mdmarufsarker@users.noreply.github.com>
2022-02-22 13:25:04 +0600

    Add files via upload

---
+++ Beginner/1001.Extremely-Basic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     long long A,B,X;
+     cin >> A >> B;
+     X = A + B;
+     cout << "X = " << X << endl;
+ 
+    return 0;
+}
