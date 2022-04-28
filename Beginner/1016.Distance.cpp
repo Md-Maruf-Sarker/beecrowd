7fea297747f6654b22754fe66b4ee1fd599e7034
Md. Maruf Sarker <78826405+mdmarufsarker@users.noreply.github.com>
2022-04-28 13:15:32 +0600

    Distance Problem Solved

---
+++ Beginner/1016.Distance.cpp
@@ -0,0 +1,17 @@
+// Distance
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int distance, output;
+	
+	cin >> distance;
+	
+	output = distance * 2;
+	
+	cout << output << " minutos\n";
+	
+    return 0;
+}
