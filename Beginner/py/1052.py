b8f760f75a28b8e380836d3a08e2b3a4c3430fb6
Md. Maruf Sarker <78826405+mdmarufsarker@users.noreply.github.com>
2022-11-05 23:37:28 +0600

    Add files via upload

---
+++ Beginner/py/1052.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+match n:
+    case 1:
+        print("January")
+    case 2:
+        print("February")
+    case 3:
+        print("March")
+    case 4:
+        print("April")
+    case 5:
+        print("May")
+    case 6:
+        print("June")
+    case 7:
+        print("July")
+    case 8:
+        print("August")
+    case 9:
+        print("September")
+    case 10:
+        print("October")
+    case 11:
+        print("November")
+    case 12:
+        print("December")
+    
